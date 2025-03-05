80c357b69db6bab6e86138a1b9a72596b1b1412a
Valeria Valeria Julio <vegadiazmiguelangel94@gmail.com>
2025-03-04 20:45:00 -0500

    Update pages

--- plugins/app.js
+++ plugins/app.js
@@ -6,18 +6,18 @@ const $appStatePlugin = {
         app.config.globalProperties.$appState = reactive({
             primary: 'emerald',
             surface: 'zinc',
-            darkTheme: false,
-            codeSandbox: false,
+            darkTheme: false,    
+            codeSandbox: false,   
             sourceType: 'options-api',
             newsActive: false,
             announcement: null,
-            storageKey: 'primevue-tailwind'
+            storageKey: 'primevue-tailwind'       
         });
     }
 };
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin((nuxtApp) => {     
     nuxtApp.vueApp.component('DeferredDemo', DeferredDemo);
-    nuxtApp.vueApp.directive('code', CodeHighlight);
-    nuxtApp.vueApp.use($appStatePlugin);
+    nuxtApp.vueApp.directive('code', CodeHighlight);    
+    nuxtApp.vueApp.use($appStatePlugin);    
 });
